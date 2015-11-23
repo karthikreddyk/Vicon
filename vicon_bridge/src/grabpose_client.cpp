5b3a70f2e8359aa285be2c8bc47fb1ffe56ab75b
lakehanne <patlekano@gmail.com>
2015-11-23 12:12:33 -0600

    not subscribing to callback

--- vicon_bridge/src/grabpose_client.cpp
+++ vicon_bridge/src/grabpose_client.cpp
@@ -2,16 +2,20 @@
 #include <tf/transform_listener.h>
 #include <vicon_bridge/viconGrabPose.h>
 #include <geometry_msgs/Pose.h>
+#include <ros/service_client.h>
+#include "ros/connection.h"
+#include "ros/service_manager.h"
+#include "ros/service.h"
+#include <ros/datatypes.h>
+#include <ros/spinner.h>
+#include <sstream>
 #include <cstdlib>
 #include <stdio.h>
 
 std::string subject, segment;
+const std::string grabber = "grabpose_clent";
 
-std::string M_string()
-{
-	std::string key = "Successful connection. Ose Patrick!"
-	return key;
-}
+// typedef std::map<std::string,std::string>ros::M_string;
 
 /*    // Service Server
     ROS_INFO("setting up grab_vicon_pose service server ... ");
@@ -19,13 +23,11 @@ std::string M_string()
                                                                 this);*/
     	//This is the service I want to subscribe to. Line 618. vicon_bridge.cpp.
 
-int main(int argc, char** argv[])
+int main(int argc, char** argv)
 {
-	ros::init(argc, argv[], "cpp_listener");
+	uint32_t options = 0;
 
-	const std::string& service_name = "grab_vicon_pose";
-	bool persistent = false;
-	const M_string& header_values = M_string();
+	ros::init(argc, argv, grabber, options);
 
 	if (argc != 3)
 	  {
@@ -33,35 +35,54 @@ int main(int argc, char** argv[])
 	    return 1;
 	  }
 
-	subject = argv[1];
-	segment = argv[2];
+  	for(size_t i = 1; i < (size_t)argc; ++i)
+  	{
+    	std::string param(argv[i]);
+
+		subject = argv[1];
+		segment = argv[2];
+	}
 
 	std::string base_name = "vicon";
 
 	std::string advertname = base_name + "/" + subject + "/" + segment;
 
+	ROS_INFO_STREAM("base_name" << "/subject" << "/segment: \t" << base_name <<  "/" << subject  << "/" << segment);
+
 	ros::NodeHandle nc;
-	ros::service::waitForService(service_name);
-	// ros::ServiceClient vicon_pose = 	nc.serviceClient<vicon_bridge::viconGrabPose>(service_name);
-	ros::ServiceClient vicon_pose = 	nc.serviceClient(service_name, persistent, \
-												header_values)			[inline]
+
+	std::ostringstream service_name;
+	service_name << base_name <<  "/" << subject  << "/" << segment;
+
+	ros::service::waitForService(service_name.str());
+
+	//ros::ServiceClient vicon_pose = 	nc.serviceClient<vicon_bridge::viconGrabPose>(service_name.str());		//Does not sunscribe to service
+/*		ServiceClient ros::NodeHandle::serviceClient	(	const std::string & 	service_name,
+															bool 	persistent = false,
+															const M_string & 	header_values = M_string() 
+															)		[inline]*/	
+	bool persistent = false;
+	const ros::M_string & 	header_values = ros::M_string();															
+	ros::ServiceClient vicon_pose =	nc.serviceClient<vicon_bridge::viconGrabPose>(service_name.str());
+
+	ros::Rate looper(10);	
+
 	vicon_bridge::viconGrabPose srv;
-
-	vicon_pose.call(srv);
-
-
-
-	srv.request.subject_name =  subject;
-	srv.request.segment_name =  segment;
-	srv.request.n_measurements = 1000;
-	if (vicon_pose.call(srv))
+	do
 	{
-		ROS_INFO_STREAM("Pose: " << srv.response.pose.pose.position.x << ")");
+		srv.request.subject_name =  subject; 
+		srv.request.segment_name =  segment;
+		srv.request.n_measurements = 1000;
+		if (vicon_pose.call(srv))
+		{
+			ROS_INFO_STREAM("Pose: " << srv.response << ")");
+		}
+		else
+		{
+			ROS_ERROR("Failed to call service viconGrabPose");
+			return 1;
+		}
 	}
-	else
-	{
-		ROS_ERROR("Failed to call service viconGrabPose");
-		return 1;
-	}
+	while(ros::ok());
 	return 0;
 }
